8f7f5e9f350125687a1873ae6935792ab26e77a6
justinenorie <justinedelacruz97@gmail.com>
2024-11-27 21:02:03 +0800

    Added a hides the Firebase Token

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,17 +4,11 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
+import firebaseConfig from "./firebasekey";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCSxMoC0UmEprBwHPV42uWXF5E03WcPVzo",
-    authDomain: "waterlevelderictory.firebaseapp.com",
-    databaseURL: "https://waterlevelderictory-default-rtdb.firebaseio.com",
-    projectId: "waterlevelderictory",
-    storageBucket: "waterlevelderictory.firebasestorage.app",
-    messagingSenderId: "275405352702",
-    appId: "1:275405352702:web:3a1672f7e14d60e667ffaf",
-    measurementId: "G-ZPV53T6H1C",
-};
+// const firebaseConfig = {
+    
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
