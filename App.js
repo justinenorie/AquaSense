51565fcf560b5b0dd4a4a1bda1113ec070d8e2a9
justinenorie <justinedelacruz97@gmail.com>
2024-12-12 02:51:50 +0800

    Fixed the bugs in fonts

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
-import FontsLoad from "./src/assets/fonts/fontsLoad";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { ActivityIndicator, View } from 'react-native';
+
 import HomePage from "./src/screens/Home";
 import LoginPage from "./src/screens/Login";
 import SignUpPage from "./src/screens/SignUp";
 import AboutUs from "./src/screens/AboutUs";
 import Dashboard from "./src/screens/Dashboard";
+import MemberProfile from "./src/screens/MemberProfile";
 
 //TODO: Add navigations here
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     const [activeState, setActiveState] = useState("Dashboard");
-    <FontsLoad />
+
+    const [fontsLoaded] = useFonts({
+        'Poppins-Black': require('./src/assets/fonts/Poppins-Black.ttf'),
+        'Poppins-Bold': require('./src/assets/fonts/Poppins-Bold.ttf'),
+        'Poppins-Regular': require('./src/assets/fonts/Poppins-Regular.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" color="#0000ff" />
+            </View>
+        );
+    }
+
     return (
         <NavigationContainer>
             <Stack.Navigator
@@ -26,6 +43,7 @@ export default function App() {
                 <Stack.Screen name="Home" component={HomePage} />
                 <Stack.Screen name="Login" component={LoginPage} />
                 <Stack.Screen name="SignUp" component={SignUpPage} />
+                <Stack.Screen name="MemberProfile" component={MemberProfile} />
 
                 {/* Menu Navigations */}
                 <Stack.Screen name="Dashboard">
---
+++ src/constants/typography.js
@@ -0,0 +1,10 @@
+const fonts = {
+    h1: { fontFamily: 'Poppins-Black', fontSize: 40 },
+    h2: { fontFamily: 'Poppins-Bold', fontSize: 30 },
+    h3: { fontFamily: 'Poppins-Regular', fontSize: 20 },
+    small: { fontFamily: 'Poppins-Regular', fontSize: 15},
+
+    membername: { fontFamily: 'Poppins-Bold', fontSize: 20 },
+    memberassign: { fontFamily: 'Poppins-Bold', fontSize: 15 },
+};
+export default fonts;
